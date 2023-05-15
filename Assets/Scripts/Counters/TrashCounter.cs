5ad4f087339b546c8aa99936cc04ae45789d6d93
Silvision <richardmnguyen@hotmail.com>
2023-05-15 01:48:25 -0500

    made trash counter

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrashCounter : BaseCounter {
-    
-    
+    public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
 }
