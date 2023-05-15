3eda63b8bcaebe27b3b6e45a0e76143baa960ee6
Silvision <richardmnguyen@hotmail.com>
2023-05-15 15:27:49 -0500

    added stove counter and visual

--- Assets/Scripts/Counters/CuttingCounter.cs
+++ Assets/Scripts/Counters/CuttingCounter.cs
@@ -4,13 +4,9 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class CuttingCounter : BaseCounter {
+public class CuttingCounter : BaseCounter, IHasProgress {
 
-    public event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
-
-    public class OnProgressChangedEventArgs : EventArgs {
-        public float progressNormalized;
-    }
+    public event EventHandler<IHasProgress.OnProgressChangedEventArgs> OnProgressChanged;
 
     public event EventHandler OnCut;
 
@@ -30,7 +26,7 @@ public class CuttingCounter : BaseCounter {
                     cuttingProgress = 0;
                     
                     CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
-                    OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs() {
+                    OnProgressChanged?.Invoke(this, new IHasProgress.OnProgressChangedEventArgs() {
                         progressNormalized = (float) cuttingProgress / cuttingRecipeSO.cuttingProgressMax
                     });
                 }
@@ -56,7 +52,7 @@ public class CuttingCounter : BaseCounter {
             
             OnCut?.Invoke(this, EventArgs.Empty);
             CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
-            OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs() {
+            OnProgressChanged?.Invoke(this, new IHasProgress.OnProgressChangedEventArgs() {
                 progressNormalized = (float) cuttingProgress / cuttingRecipeSO.cuttingProgressMax
             });
 
