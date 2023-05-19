3ad58ced9b5a5568822efae14cc6f4cb63a54db2
Silvision <richardmnguyen@hotmail.com>
2023-05-19 02:07:28 -0500

    pause and clear statics added

--- Assets/Scripts/KitchenGameManager.cs
+++ Assets/Scripts/KitchenGameManager.cs
@@ -9,6 +9,9 @@ public class KitchenGameManager : MonoBehaviour {
     public static KitchenGameManager Instance { get; private set; }
 
     public event EventHandler OnStateChanged;
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameUnpaused;
+    
     
     private enum State {
         WaitingToStart,
@@ -22,7 +25,7 @@ public class KitchenGameManager : MonoBehaviour {
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
     private float gamePlayingTimerMax = 10f;
-
+    private bool isGamePaused = false;
     
 
     private void Awake() {
@@ -31,6 +34,14 @@ public class KitchenGameManager : MonoBehaviour {
         state = State.WaitingToStart;
     }
 
+    private void Start() {
+        GameInput.Instance.OnPauseAction += GameInput_OnPauseAction;
+    }
+
+    private void GameInput_OnPauseAction(object sender, EventArgs e) {
+        TogglePauseGame();
+    }
+    
     private void Update() {
         switch (state) {
             case State.WaitingToStart:
@@ -80,5 +91,17 @@ public class KitchenGameManager : MonoBehaviour {
 
     public float GetGamePlayingTimerNormalized() {
         return 1 - (gamePlayingTimer / gamePlayingTimerMax);
+    }
+    
+    public void TogglePauseGame() {
+        isGamePaused = !isGamePaused;
+        if (isGamePaused) {
+            Time.timeScale = 0f;    
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
+        } else {
+            Time.timeScale = 1f;
+            OnGameUnpaused?.Invoke(this, EventArgs.Empty);
+        }
+        
     }
 }
