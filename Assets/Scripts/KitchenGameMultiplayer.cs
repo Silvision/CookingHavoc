35168f5c60bf51b17fe466eadb0b95c8b783a4c2
Silvision <richardmnguyen@hotmail.com>
2023-05-25 19:31:48 -0500

    sync pick up objects

---
+++ Assets/Scripts/KitchenGameMultiplayer.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class KitchenGameMultiplayer : NetworkBehaviour {
+
+    [SerializeField] private KitchenObjectListSO kitchenObjectListSO;
+    
+    public static KitchenGameMultiplayer Instance { get; private set; }
+
+    private void Awake() {
+        Instance = this;
+    }
+    
+    
+    public void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
+        SpawnKitchenObjectServerRpc(GetKitchenObjectSOIndex(kitchenObjectSO), kitchenObjectParent.GetNetworkObject());
+    }
+    
+    [ServerRpc(RequireOwnership = false)]
+    private void SpawnKitchenObjectServerRpc(int kitchenObjectSOIndex, NetworkObjectReference kitchenObjectParentNetworkObjectReference) {
+        KitchenObjectSO kitchenObjectSO = GetKitchenObjectSOFromIndex(kitchenObjectSOIndex);
+        
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+
+        NetworkObject kitchenObjectNetworkObject = kitchenObjectTransform.GetComponent<NetworkObject>();
+        kitchenObjectNetworkObject.Spawn(true);
+        
+        KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+
+        kitchenObjectParentNetworkObjectReference.TryGet(out NetworkObject kitchenObjectParentNetworkObject);
+        IKitchenObjectParent kitchenObjectParent = kitchenObjectParentNetworkObject.GetComponent<IKitchenObjectParent>();
+        kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
+    }
+
+    private int GetKitchenObjectSOIndex(KitchenObjectSO kitchenObjectSO) {
+        return kitchenObjectListSO.kitchenObjectSOList.IndexOf(kitchenObjectSO);
+    }
+
+    private KitchenObjectSO GetKitchenObjectSOFromIndex(int kitchenObjectSOIndex) {
+        return kitchenObjectListSO.kitchenObjectSOList[kitchenObjectSOIndex];
+    }
+    
+}
