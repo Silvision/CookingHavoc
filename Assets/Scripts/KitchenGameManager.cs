052f5b0537e9c780e6c996869d0160c0b518d27f
Silvision <richardmnguyen@hotmail.com>
2023-06-01 13:31:43 -0500

    handle late joins

--- Assets/Scripts/KitchenGameManager.cs
+++ Assets/Scripts/KitchenGameManager.cs
@@ -158,6 +158,10 @@ public class KitchenGameManager : NetworkBehaviour {
         return state.Value == State.GameOver;
     }
 
+    public bool IsWaitingToStart() {
+        return state.Value == State.WaitingToStart;
+    }
+
     public bool IsLocalPlayerReady() {
         return isLocalPlayerReady;
     }
